192dce885fb8d7eebf6304f1281cf89fa556bea8
petryshend <petryshend@gmail.com>
2015-06-21 15:47:30 +0300

    Edit AnswerUnit

--- src/RachinskyAdminBundle/Controller/AnswerUnitsController.php
+++ src/RachinskyAdminBundle/Controller/AnswerUnitsController.php
@@ -11,7 +11,16 @@ class AnswerUnitsController extends Controller {
 
     public function listAction()
     {
-        return $this->render(':admin/answer-units:list.html.twig');
+        $units = $this->getDoctrine()
+            ->getRepository('RachinskyAdminBundle:AnswerUnit')
+            ->findAll();
+
+        return $this->render(
+            ':admin/answer-units:list.html.twig',
+            [
+                'units' => $units,
+            ]
+        );
     }
 
     public function newAction(Request $request)
@@ -37,7 +46,28 @@ class AnswerUnitsController extends Controller {
 
     public function editAction(Request $request, $id)
     {
-        return $this->render('admin/answer-units/edit.html.twig', ['id' => $id]);
+        $unit = $this->getDoctrine()
+            ->getRepository('RachinskyAdminBundle:AnswerUnit')
+            ->find($id);
+        if (!$unit) {
+            throw new \Exception('AnswerUnit with that ID does not exist');
+        }
+        $form = $this->createForm(new UnitType(), $unit);
+        $form->handleRequest($request);
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->flush();
+
+            return $this->redirectToRoute('rachinsky_admin_answer_units_list');
+        }
+
+        return $this->render(
+            'admin/answer-units/edit.html.twig',
+            [
+                'unit' => $unit,
+                'form' => $form->createView(),
+            ]
+        );
     }
 
     public function deleteAction(Request $request, $id)
